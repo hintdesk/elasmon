0ef5a4ffe5f4e1e460110fc0b32866cb4e36cf68
LIEBHERR\lisngt0 <tri.nguyen@liebherr.com>
2026-01-06 09:31:28 +0100

    Replace main-content class with flex-1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, signal, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenubarModule } from 'primeng/menubar';
 import { ConnectionComponent } from './components/connection/connection.component';
@@ -8,21 +8,35 @@ import { IndexComponent } from './components/index/index.component';
 import { ShardComponent } from './components/shard.component/shard.component';
 import { MenuItem } from 'primeng/api';
 import { EsConnection } from './entities/esConnection';
+import { ButtonModule } from 'primeng/button';
 import { PanelMenuModule } from 'primeng/panelmenu';
 import { ConnectionsComponent } from './components/connections/connections.component';
 import { ConnectionService } from './services/connection.service';
 import packageJson from '../../package.json';
 
+const DESKTOP_BREAKPOINT = 1024; // Tailwind lg breakpoint
+
 @Component({
   selector: 'app-root',
-  imports: [ConnectionsComponent, PanelMenuModule ,ShardComponent, IndexComponent, NodeComponent, SplitterModule, ConnectionComponent, MenubarModule, RouterOutlet],
+  imports: [ConnectionsComponent, PanelMenuModule, ButtonModule, ShardComponent, IndexComponent, NodeComponent, SplitterModule, ConnectionComponent, MenubarModule, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   version = packageJson.version;
   selectedConnectionNode = signal<any | undefined>(undefined);
   connectionNodes = computed<MenuItem[]>(() => this.buildConnectionNodes());
+  // Sidebar visibility - Desktop: default visible, Mobile/Tablet: default hidden
+  sidebarVisible = signal<boolean>(true);
+
+  ngOnInit() {
+    // Set default based on screen size
+    this.sidebarVisible.set(window.innerWidth >= DESKTOP_BREAKPOINT);
+  }
+
+  toggleSidebar() {
+    this.sidebarVisible.set(!this.sidebarVisible());
+  }
 
   constructor(private connectionService: ConnectionService) {
 
